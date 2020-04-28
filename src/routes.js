c19ac9710b97acb777ed2eea8a81df1bc0a0d171
Jonathas <jhsalves@gmail.com>
2020-04-27 21:26:34 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -29,20 +29,16 @@ routes.post("/files", upload.single("file"), FileController.store);
 routes.post("/sessions", SessionController.store);
 
 routes.post("/users", UserController.store);
+routes.get("/users",authMiddleware, UserController.index);
+routes.put("/users/profiles", authMiddleware, UserController.setUsersProfile);
+routes.get("/users/profiles", authMiddleware, UserController.listProfiles);
 
 routes.post("/voluntary", VolunteersController.show);
-
 routes.get("/volunteers",authMiddleware, VolunteersController.index);
-routes.get("/users",authMiddleware, UserController.index);
-
 routes.post("/volunteers",authMiddleware, VolunteersController.store);
 routes.put("/volunteers",authMiddleware, VolunteersController.update);
 
 routes.put("/quitVolunteer", VolunteersController.updateVolunteer);
-
-routes.put("/users/profiles", authMiddleware, UserController.setUsersProfile)
-routes.get("/users/profiles", authMiddleware, UserController.listProfiles)
-routes.post("/files", upload.single("file"), FileController.store);
 
 routes.get("/help", HelpRequestController.index);
 routes.post("/help", HelpRequestController.store);
